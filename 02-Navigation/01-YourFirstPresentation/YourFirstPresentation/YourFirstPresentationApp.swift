bbf999c9d93bf2b385999373841434e60e8aa991
Sasha Jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-08-07 00:22:22 +0300

    Your first presentation: Project set up

---
+++ 02-Navigation/01-YourFirstPresentation/YourFirstPresentation/YourFirstPresentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourFirstPresentationApp.swift
+//  YourFirstPresentation
+//
+//  Created by Sasha Jaroshevskii on 06.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct YourFirstPresentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 02-Navigation/01-YourFirstPresentation/YourFirstPresentationTests/YourFirstPresentationTests.swift
@@ -0,0 +1,17 @@
+//
+//  YourFirstPresentationTests.swift
+//  YourFirstPresentationTests
+//
+//  Created by Sasha Jaroshevskii on 06.08.2025.
+//
+
+import Testing
+@testable import YourFirstPresentation
+
+struct YourFirstPresentationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
