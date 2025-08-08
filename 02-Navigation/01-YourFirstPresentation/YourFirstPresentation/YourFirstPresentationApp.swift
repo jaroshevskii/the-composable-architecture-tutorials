cb5ea9515174f79de3ac06ec4d2c2a8143f5e282
Sasha Jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-08-08 23:56:58 +0300

    Updates

--- 02-Navigation/01-YourFirstPresentation/YourFirstPresentation/YourFirstPresentationApp.swift
+++ 02-Navigation/01-YourFirstPresentation/YourFirstPresentation/YourFirstPresentationApp.swift
@@ -5,13 +5,18 @@
 //  Created by Sasha Jaroshevskii on 06.08.2025.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct YourFirstPresentationApp: App {
+    static let store = Store(initialState: ContactsFeature.State()) {
+        ContactsFeature()
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContactsView(store: Self.store)
         }
     }
 }
